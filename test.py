f28490d2afca47275f7f0d773820815cf1641583
Lucas Pereira Cavalcante <lukaspc003@gmail.com>
2024-06-09 23:08:57 -0300

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from hello import hello
 
 class Testes(unittest.TestCase):
 	def test_func(self):
-		self.assertEqual(hello(), "Teste")
+		self.assertEqual(hello(), "Teste da API")
 
 if __name__ == '__main__':
 	unittest.main()
